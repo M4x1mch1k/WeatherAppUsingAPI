8814d14ca41ac5e750c0b1ebec8b4b1acf8c17c3
Max Kozlov <68003929+M4x1mch1k@users.noreply.github.com>
2022-06-28 00:44:53 +0300

    Update weather_visual.py

--- weather_visual.py
+++ weather_visual.py
@@ -1,9 +1,9 @@
 from tkinter import *
 import tkinter
 import requests
-from datetime import datetime
 
 
+# Clear button
 def clear():
     E1.delete(0, 'end')
     E2.delete(0, 'end')
@@ -13,9 +13,11 @@ def clear():
 
 
 def weather():
-    user_api = 'Your user API'
+    # Your own uniqe API
+    user_api = 'cd7638ec7729ec904e9c58b23cf42891'
     location = Entry.get(E1)
 
+    # Complete link to site with your API
     complete_api = "http://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=" + user_api
 
     api_link = requests.get(complete_api)
@@ -28,7 +30,6 @@ def weather():
         weather_desc = api_data['weather'][0]['description']
         hmdt = api_data['main']['humidity']
         wind_speed = api_data['wind']['speed']
-        date_time = datetime.now().strftime("%d %b %Y | %I:%M:%S %p")
 
         temp_city_1 = temp_city, "C"
         Entry.insert(E2, 0, temp_city_1)
@@ -39,26 +40,28 @@ def weather():
         Entry.insert(E5, 0, wind_speed_1)
 
 
-top = tkinter.Tk()
-L1 = Label(top, text="The Weather App", ).grid(row=0, column=1)
-L2 = Label(top, text="City", ).grid(row=1, column=0)
-E1 = Entry(top, bd=5)
+# Creating simple visual representaion using Tkinter
+Weather = tkinter.Tk()
+Weather.title("Weather")
+L1 = Label(Weather, text="The Weather App", ).grid(row=0, column=1)
+L2 = Label(Weather, text="City", ).grid(row=1, column=0)
+E1 = Entry(Weather, bd=5)
 E1.grid(row=1, column=1)
-L3 = Label(top, text="Weather Details", ).grid(row=2, column=1)
-L3 = Label(top, text="Temperature", ).grid(row=3, column=0)
-E2 = Entry(top, bd=5)
+L3 = Label(Weather, text="Weather Details", ).grid(row=2, column=1)
+L3 = Label(Weather, text="Temperature", ).grid(row=3, column=0)
+E2 = Entry(Weather, bd=5)
 E2.grid(row=3, column=1)
-L4 = Label(top, text="Description", ).grid(row=4, column=0)
-E3 = Entry(top, bd=5)
+L4 = Label(Weather, text="Description", ).grid(row=4, column=0)
+E3 = Entry(Weather, bd=5)
 E3.grid(row=4, column=1)
-L5 = Label(top, text="Humidity", ).grid(row=5, column=0)
-E4 = Entry(top, bd=5)
+L5 = Label(Weather, text="Humidity", ).grid(row=5, column=0)
+E4 = Entry(Weather, bd=5)
 E4.grid(row=5, column=1)
-L5 = Label(top, text="Wind Speed", ).grid(row=6, column=0)
-E5 = Entry(top, bd=5)
+L5 = Label(Weather, text="Wind Speed", ).grid(row=6, column=0)
+E5 = Entry(Weather, bd=5)
 E5.grid(row=6, column=1)
 
-B = Button(top, text="Submit", command=weather).grid(row=7, column=1, )
-B = Button(top, text="Clear", command=clear).grid(row=7, column=2, )
+B = Button(Weather, text="Submit", command=weather).grid(row=7, column=1, )
+B = Button(Weather, text="Clear", command=clear).grid(row=7, column=2, )
 
-top.mainloop()
+Weather.mainloop()
